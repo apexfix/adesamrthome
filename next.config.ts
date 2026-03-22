cbe69b80f27ae828dff48b146f66a438a06d1136
apexfix <134393835+apexfix@users.noreply.github.com>
2026-03-22 20:10:48 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,18 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['nodemailer'],
   images: {
     remotePatterns: [
+      // 保留你原本的 IP 图片源
       {
         protocol: "http",
         hostname: "45.145.229.20",
         port: "2031",
         pathname: "/**",
+      },
+      // 新增 Cloudinary 官方图床白名单
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
       },
     ],
   },
