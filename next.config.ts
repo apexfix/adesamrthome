a30d3f11c07ccb7e1561f0104fb666363d225239
apexfix <sunzhonglin2000@gmail.com>
2026-08-16 20:06:29 +0930

    Allow Shopify product images

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,11 @@ const nextConfig: NextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        pathname: '/**',
+      },
     ],
   },
 };
